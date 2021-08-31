c61fedc8ff11e258dc12ea5c9e60b749c4d2ace7
Roman Nazarenko <me@jtalk.me>
2021-08-31 16:37:14 +0100

    Decide domain selection on the TF side

--- jtalk-me/apps.tf
+++ jtalk-me/apps.tf
@@ -2,7 +2,7 @@
 module "jtalkme_live" {
   source         = "./modules/jtalk.me"
   namespace      = "home"
-  domains        = var.domains
+  domains        = flatten([for d in var.domains : [d, "www.${d}"]])
   database_url   = "${local.atlas_cluster_url_parts.0}://${mongodbatlas_database_user.app_user["home"].username}:${mongodbatlas_database_user.app_user["home"].password}@${local.atlas_cluster_url_parts.1}/home"
   backup_bucket  = "${aws_s3_bucket.jtalkme_backup.bucket}/new"
   aws_key_id     = aws_iam_access_key.jtalkme_backup.id
@@ -15,7 +15,7 @@ module "jtalkme_staging" {
   source       = "./modules/jtalk.me"
   namespace    = "home-staging"
   staging      = true
-  domains      = var.domains
+  domains      = [for d in var.domains : "staging.${d}"]
   database_url = "${local.atlas_cluster_url_parts.0}://${mongodbatlas_database_user.app_user["home-staging"].username}:${mongodbatlas_database_user.app_user["home-staging"].password}@${local.atlas_cluster_url_parts.1}/home-staging"
   app_version  = var.jtalkme_staging_version
 }
