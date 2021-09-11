894f45eca04051c428eab713fa7d9691c6d96c55
Roman Nazarenko <me@jtalk.me>
2021-09-11 15:26:49 +0100

    Relax deploy timeouts for jtalk.me

--- jtalk-me/modules/jtalk.me/app.tf
+++ jtalk-me/modules/jtalk.me/app.tf
@@ -8,7 +8,7 @@ resource "helm_release" "jtalk_me" {
   create_namespace = true
 
   atomic        = true
-  timeout       = 600
+  timeout       = 60
   wait_for_jobs = true
 
   cleanup_on_fail = true
