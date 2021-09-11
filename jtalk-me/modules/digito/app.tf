5f1e76c1e9ca0aeb456fafa6f9a538949ea5cb41
Roman Nazarenko <me@jtalk.me>
2021-09-11 15:27:10 +0100

    Relax deploy timeouts for digito

--- jtalk-me/modules/digito/app.tf
+++ jtalk-me/modules/digito/app.tf
@@ -8,7 +8,7 @@ resource "helm_release" "digito" {
   create_namespace = true
 
   atomic        = true
-  timeout       = 900
+  timeout       = 60
   wait_for_jobs = true
 
   cleanup_on_fail = true
