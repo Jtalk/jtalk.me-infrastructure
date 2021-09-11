77a0189bdc1a8fe58bf30aabdc4425979517a9ef
Roman Nazarenko <me@jtalk.me>
2021-09-11 15:26:34 +0100

    Fix metrics server to work in Digitalocean

--- jtalk-me/cluster.tf
+++ jtalk-me/cluster.tf
@@ -31,10 +31,24 @@ resource "helm_release" "metrics_server" {
   name       = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "metrics-server"
+  version    = "~> 4"
+
+  namespace        = "metrics-server"
+  create_namespace = true
 
   atomic  = true
-  timeout = 300
+  timeout = 60
 
   cleanup_on_fail = true
   max_history     = 2
+
+  set {
+    name  = "extraArgs.kubelet-preferred-address-types"
+    value = "InternalIP"
+  }
+
+  set {
+    name  = "apiService.create"
+    value = true
+  }
 }
