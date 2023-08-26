fad29dedf588f4082c5834450fa07ec6fc305f6c
Roman Nazarenko <me@jtalk.me>
2023-08-26 16:17:23 +0100

    Add redirect to www for the cloud domains.

--- jtalk-me/modules/dns/modules/dns-cloud/dns.tf
+++ jtalk-me/modules/dns/modules/dns-cloud/dns.tf
@@ -3,5 +3,26 @@ resource "cloudflare_record" "root" {
   name    = "www"
   type    = "CNAME"
   value   = var.cloud_cname
-  proxied = false
+  proxied = true
+}
+
+resource "cloudflare_record" "redirect_root" {
+  zone_id = var.zone_id
+  name    = var.root_domain
+  type    = "CNAME"
+  value   = var.cloud_cname
+  proxied = true
+}
+
+resource "cloudflare_page_rule" "redirect_to_www" {
+  zone_id  = var.zone_id
+  target   = "${var.root_domain}/*"
+  priority = 2
+
+  actions {
+    forwarding_url {
+      url         = "https://www.${var.root_domain}/$1"
+      status_code = 302
+    }
+  }
 }
